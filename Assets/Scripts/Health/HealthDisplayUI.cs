45f77ba492eb27432a9e1c3c249879bca96432c5
Matt Burns <mmburns88@gmail.com>
2023-01-28 13:16:51 -0500

    added a graphical display for player health

--- Assets/Scripts/Health/HealthDisplayUI.cs
+++ Assets/Scripts/Health/HealthDisplayUI.cs
@@ -1,13 +1,19 @@
+using Player;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Health
 {
     public class HealthDisplayUI : MonoBehaviour
     {
         [SerializeField] private HealthController healthController;
+        [SerializeField] private Color maxHealthColor;
+        [SerializeField] private Color minHealthColor;
         
         private TMP_Text _text;
+        private Image _iconPlayer;
+        private Image _icon;
 
         private void Awake()
         {
@@ -15,12 +21,44 @@ namespace Health
 
             if (_text == null)
                 _text = GetComponentInChildren<TMP_Text>();
+
+            if (healthController.GetComponent<PlayerController>())
+                _iconPlayer = GetComponent<Image>();
+            else
+                _icon = GetComponent<Image>();
         }
 
-        private void OnEnable() => healthController.onUpdateHealthDisplay += UpdateHealthDisplay;
+        private void Start() => UpdateHealthDisplay();
+
+        private void OnEnable()
+        {
+            healthController.onUpdateHealthDisplay += UpdateHealthDisplay;
+            healthController.onUpdateHealthDisplay.Invoke();
+        }
 
         private void OnDisable() => healthController.onUpdateHealthDisplay -= UpdateHealthDisplay;
 
-        public void UpdateHealthDisplay() => _text.text = healthController.GetHealth().ToString();
+        public void UpdateHealthDisplay()
+        {
+            if (_text != null)
+                _text.text = healthController.GetHealth().ToString();
+            
+            if (_iconPlayer != null)
+            {
+                var maxHealth = healthController.GetMaxHealth();
+                var twentyPercent = maxHealth * 0.2f;
+                var eightyPercent = maxHealth * 0.8f;
+                var currentHealth = healthController.GetHealth();
+
+                if (currentHealth >= twentyPercent)
+                {
+                    var percentage = (currentHealth - twentyPercent) / eightyPercent;
+                    _iconPlayer.fillAmount = 1f;
+                    _iconPlayer.color = Color.Lerp(minHealthColor, maxHealthColor, percentage);
+                }
+                else
+                    _iconPlayer.fillAmount = currentHealth / twentyPercent;
+            }
+        }
     }
 }
